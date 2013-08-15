d561321f91d383aebee533db569bdd55b92825f7
Joel Shin <jwshin@gmail.com>
2013-08-15 14:44:34 -0700

    added basic http access key security to api

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
-    class CategoriesController < ApplicationController
+    class CategoriesController < ApplicationController  
+      #skip_before_filter :verify_authenticity_token, :only => [:create]
+                                                                               
+      before_filter :restrict_access
+      
       respond_to :json
             
       def index
@@ -20,6 +24,16 @@ module Api
         respond_with Category.destroy(params[:id])
       end
       
+    private
+      #def restrict_access
+      #  api_key = ApiKey.find_by_access_token(params[:access_token])
+      #  head :unauthorized unless api_key
+      #end
+      def restrict_access
+        authenticate_or_request_with_http_token do |token, options|
+          ApiKey.exists?(access_token: token)
+        end
+      end  
     end
   end
 end
