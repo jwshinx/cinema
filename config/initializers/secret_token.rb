938ba91b1ff5140800204b60cb9849dd488a987a
Joel Shin <jwshin@gmail.com>
2013-08-14 21:39:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinema::Application.config.secret_token = '54e989ccfcff12d3d7d333b9b0ff22ccf6c2b93a70fadc1cd6da0d0027923a8a2a4f592abf5f8ab0dd0e35c2e7bb1b0d41864e24bb593b3478418a2c12527e7e'
